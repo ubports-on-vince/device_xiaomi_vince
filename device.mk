2282b49b55f73923634d40dbd18b25e19d815c1c
Bartek <bartek96@tuta.io>
2019-01-31 08:39:41 +0100

    disable qcom FM to avoid error
    Change-Id: Ib873829f15c9d5fd5ee2df6320ddd6bcc5f2704f

--- device.mk
+++ device.mk
@@ -64,6 +64,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+PRODUCT_PACKAGES += \
+    libcurl
+
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
