fcbd7b5d2f280b73038f7b80fe2cbe5a1473b40e
BirdZhang <0312birdzhang@gmail.com>
2019-11-13 02:27:43 +0000

    enable wifi display

--- device.mk
+++ device.mk
@@ -355,4 +355,5 @@ MINIMEDIA_SENSORSERVER_DISABLE := 1
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.t-o.quirk.forcesink=sink.primary_output \
     ro.t-o.quirk.forcesource=source.record_24_primary_input \
-    ro.qc.sensors.wl_dis=true
+    ro.qc.sensors.wl_dis=true \
+    ubuntu.widi.supported=true
